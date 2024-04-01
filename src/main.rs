abb3006e2385c1759ec2a6c375d52d36b8e8a3d8
victor <leonn366@gmail.com>
2024-04-01 15:15:45 +0300

    init 2

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,126 @@
-use eframe::egui;
-use eframe::egui::{Button, FontData, FontDefinitions, FontFamily, FontId};
+use std::collections::HashMap;
 
-use eframe::egui::{ TextStyle};
-use eframe::egui::FontFamily::{Proportional};
-use eframe::egui::TextStyle::{Body, Heading, Small}; // Import TextStyle here
+use eframe::egui::{RichText, Ui};
+use egui::{Align, Button, Layout};
+use egui_json_tree::{DefaultExpand, JsonTree};
+use serde_json::{json, Value};
 
-use egui_json_tree::{DefaultExpand, JsonTree, JsonTreeStyle};
+trait Show {
+  fn title(&self) -> &'static str;
+  fn show(&mut self, ui: &mut Ui);
+}
 
+
+struct CustomExample {
+  title: &'static str,
+  input: String,
+}
+
+impl CustomExample {
+  fn new(title: &'static str) -> Self {
+    Self {
+      title,
+      input: serde_json::to_string_pretty(&json!({"foo": "bar"})).unwrap(),
+    }
+  }
+}
+
+impl Show for CustomExample {
+  fn title(&self) -> &'static str {
+    self.title
+  }
+
+  fn show(&mut self, ui: &mut Ui) {
+    ui.label("Enter raw JSON in the text box to see the visualisation below.");
+
+    ui.add_space(ui.spacing().item_spacing.y);
+    ui.add(
+      egui::TextEdit::multiline(&mut self.input)
+        .code_editor()
+        .desired_rows(4)
+        .desired_width(f32::INFINITY),
+    );
+
+    let value: serde_json::Result<Value> = serde_json::from_str(&self.input);
+    let pretty_string = value
+      .as_ref()
+      .ok()
+      .and_then(|v| serde_json::to_string_pretty(v).ok());
+
+    ui.add_space(ui.spacing().item_spacing.y);
+    ui.add_enabled_ui(pretty_string.is_some(), |ui| {
+      if ui.button("Beautify").clicked() {
+        self.input = pretty_string.unwrap();
+      }
+    });
+
+    ui.add_space(ui.spacing().item_spacing.y);
+    ui.separator();
+
+    match value.as_ref() {
+      Ok(value) => {
+        JsonTree::new(self.title, value).show(ui);
+      }
+      Err(err) => {
+        ui.label(RichText::new(err.to_string()).color(ui.visuals().error_fg_color));
+      }
+    };
+
+    ui.add_space(ui.spacing().item_spacing.y);
+  }
+}
+
+
+
+
+struct DemoApp {
+  examples: Vec<Box<dyn Show>>,
+  open_example_titles: HashMap<&'static str, bool>,
+}
+
+impl Default for DemoApp {
+  fn default() -> Self {
+    Self {
+      examples: vec![
+        Box::new(CustomExample::new("Custom Input")),
+      ],
+      open_example_titles: HashMap::new(),
+    }
+  }
+}
+
+impl eframe::App for DemoApp {
+  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    egui::SidePanel::left("left_panel")
+      .resizable(true)
+      .show(ctx, |ui| {
+        ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {
+          for example in self.examples.iter() {
+            let is_open = self.open_example_titles.entry(example.title()).or_default();
+
+            ui.toggle_value(is_open, example.title());
+          }
+        });
+      });
+
+    for example in self.examples.iter_mut() {
+      let is_open = self.open_example_titles.entry(example.title()).or_default();
+
+      egui::Window::new(example.title())
+        .open(is_open)
+        .show(ctx, |ui| example.show(ui));
+    }
+  }
+
+  fn clear_color(&self, visuals: &egui::Visuals) -> [f32; 4] {
+    visuals.panel_fill.to_normalized_gamma_f32()
+  }
+}
 
 fn main() {
-
-
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
-
-
-
-}
-
-#[derive(Default)]
-struct MyEguiApp {}
-
-
-
-
-impl MyEguiApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-
-        Self::default()
-    }
-}
-
-impl eframe::App for MyEguiApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-
-        /*let mut style = (*ctx.style()).clone();
-        style.text_styles = [
-            (Heading, FontId::new(100.0, Proportional)),
-        ]
-          .into();
-        ctx.set_style(style);*/
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
-
-            let value = serde_json::json!({ "foo": "bar", "fizz": [1, 2, 3]});
-
-// Simple:
-            JsonTree::new("simple-tree", &value).show(ui);
-        });
-
-    }
+  let _ = eframe::run_native(
+    "egui-json-tree example",
+    eframe::NativeOptions::default(),
+    Box::new(|_cc| Box::<DemoApp>::default()),
+  );
 }
